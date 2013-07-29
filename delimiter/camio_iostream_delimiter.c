ba7f2a60a9e00a371113e4c6cd3e8455655eccde
Matthew P. Grosvenor <mgrosvenor@gmail.com>
2013-07-29 10:18:20 +0200

    Added debugging

--- delimiter/camio_iostream_delimiter.c
+++ delimiter/camio_iostream_delimiter.c
@@ -95,7 +95,12 @@ static int prepare_next(camio_iostream_delimiter_t* priv){
         //TODO XXX, can potentially avoid this if the delimiter says that data in read_buffer is a complete packet but have to deal
         //(potentially) with a partial fragment(s) left over in the buffer.
 	        
-	    printf("Adding %lu bytes at %p (offset=%lu)\n", priv->read_buffer_size, priv->working_buffer + priv->working_buffer_contents_size, priv->working_buffer_contents_size);
+        printf("Adding %lu ", priv->read_buffer_size );
+        printf("bytes at %p ", priv->working_buffer + priv->working_buffer_contents_size);
+        printf(" (offset=%lu)\n", priv->working_buffer_contents_size);
+
+
+        printf("Adding %lu bytes at %p (offset=%lu)\n", priv->read_buffer_size, priv->working_buffer + priv->working_buffer_contents_size, priv->working_buffer_contents_size);
 	    memcpy(priv->working_buffer + priv->working_buffer_contents_size, priv->read_buffer, priv->read_buffer_size);
         priv->working_buffer_contents_size += priv->read_buffer_size;
 
